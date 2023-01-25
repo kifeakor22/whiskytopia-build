d3e7a5a81a30fff877d9a09893599f614aa73799
Kc <kifeakor@yahoo.com>
2023-01-25 13:09:03 +0000

    warning alert added

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  Swal.fire({
+    color: '#F5A623',
+    title: 'Over 18\'s Only',
+    text: 'This website is intended for adults only.',
+    type: 'warning',
+    icon: 'warning',
+    iconColor:'#F5A623',
+ 
+    background: "black",
+    confirmButtonColor: '#F5A623',
+    confirmButtonText: 'Confirm',
+    contentColor: '#F5A623'
+  })
+}
